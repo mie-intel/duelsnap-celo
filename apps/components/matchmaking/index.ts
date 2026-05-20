6c0bfead9c22b9f77fe46fd69d9e72799895e0a4
mie-intel <paryapradhanika@gmail.com>
2026-05-20 15:02:30 +0700

    feat(matchmaking): barrel export + shared spring/exit animation variants

---
+++ apps/components/matchmaking/index.ts
@@ -0,0 +1,8 @@
+export { MatchmakingWidget } from "./MatchmakingWidget";
+export { MatchmakingIdle } from "./MatchmakingIdle";
+export { MatchmakingSearching } from "./MatchmakingSearching";
+export { MatchmakingFound } from "./MatchmakingFound";
+export { MatchmakingCountdown } from "./MatchmakingCountdown";
+export { MatchmakingTimeout } from "./MatchmakingTimeout";
+export { OpponentCard } from "./OpponentCard";
+export { WagerPicker } from "./WagerPicker";
