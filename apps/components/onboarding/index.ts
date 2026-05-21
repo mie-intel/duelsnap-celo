cadf0c9599b6d3fa27f8a79b934b4f6f17da2953
mie-intel <paryapradhanika.dev@gmail.com>
2026-05-21 08:53:19 +0700

    feat: barrel exports for onboarding and toast modules (#174)

---
+++ apps/components/onboarding/index.ts
@@ -0,0 +1,6 @@
+export { OnboardingProvider, useOnboarding } from "./OnboardingContext";
+export type { OnboardingStep } from "./OnboardingContext";
+export { default as OnboardingModal } from "./OnboardingModal";
+export { default as OnboardingWizard } from "./OnboardingWizard";
+export { default as OnboardingProgress } from "./OnboardingProgress";
+export { default as SkipButton } from "./SkipButton";
